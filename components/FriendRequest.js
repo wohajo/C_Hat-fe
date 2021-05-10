8591318b7ca676ffb59284ac0f7da8d6083ebe50
Michał Prawda <46087735+wohajo@users.noreply.github.com>
2021-05-10 16:29:42 +0200

    Add friend requests displaying

---
+++ components/FriendRequest.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "../styles/FriendRequest.module.scss";
+import { useEffect, useState } from "react";
+
+function FriendRequest({ id, status, timestamp, sender, receiver, isPending }) {
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    if (isPending) {
+      setUsername(() => sender.username);
+      setName(() => sender.firstName + sender.lastName);
+    } else {
+      setUsername(() => receiver.username);
+      setName(() => `${receiver.firstName} ${receiver.lastName}`);
+    }
+  });
+
+  const checkIfPending = () => {
+    if (isPending) {
+      return (
+        <div>
+          <button>A</button>
+          <button>R</button>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className={styles.friendRequest}>
+      <div className={styles.requestUsername}>{username}</div>
+      <div className={styles.requestName}>{name}</div>
+      <div className={styles.requestButtons}>{checkIfPending()}</div>
+    </div>
+  );
+}
+
+export default FriendRequest;
--- pages/friends.js
+++ pages/friends.js
@@ -3,12 +3,18 @@ import allStyles from "../styles/All.module.scss";
 import styles from "../styles/Friends.module.scss";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import FriendRequest from "../components/FriendRequest";
+import axios from "axios";
 
 export default function Friends() {
   const [session, loading] = useSession();
   const [username, setUsername] = useState("");
   const [userId, setUserId] = useState();
+  const [token, setToken] = useState("");
+
   const [currentTab, setCurrentTab] = useState("find");
+  const [friendsRequests, setFriendsRequests] = useState([]);
+  const [friends, setFriends] = useState([]);
   const router = useRouter();
 
   useEffect(async () => {
@@ -17,12 +23,13 @@ export default function Friends() {
     } else {
       await getSession().then((res) => {
         if (res !== null) {
-          setUsername(res.user.name);
-          setUserId(res.id);
+          setUsername(() => res.user.name);
+          setToken(() => res.accessToken);
+          setUserId(() => res.id);
         } else {
           router.push("/");
         }
-      })
+      });
     }
   }, [loading, session]);
 
@@ -31,36 +38,93 @@ export default function Friends() {
       if (tabName === currentTab) {
         return styles.activeButton;
       }
-      return styles.tabButton; 
+      return styles.tabButton;
     } else {
       if (tabName === currentTab) {
         return styles.activeTab;
       }
-      return styles.tab; 
+      return styles.tab;
     }
-  }
+  };
+
+  const handlePending = () => {
+    setCurrentTab(() => "pending");
+    axios
+      .get("http://localhost:8081/api/invites/my/incoming", {
+        auth: {
+          username: token,
+          password: "x",
+        },
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((res) => setFriendsRequests(() => [...res.data.invites]));
+  };
+
+  const handleSent = () => {
+    setCurrentTab(() => "sent");
+    axios
+      .get("http://localhost:8081/api/invites/my/sent", {
+        auth: {
+          username: token,
+          password: "x",
+        },
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      .then((res) => setFriendsRequests(() => [...res.data.invites]));
+  };
 
   return (
     <div className={allStyles.container}>
       <h1>Manage friends</h1>
       <div className={styles.tabContainer}>
         <div className={styles.tabButtons}>
-          <button className={checkIfActive("find", "BTN")} onClick={() => {setCurrentTab(() => "find")}}>Find</button>
-          <button className={checkIfActive("your", "BTN")} onClick={() => {setCurrentTab(() => "your")}}>Your friends</button>
-          <button className={checkIfActive("pending", "BTN")} onClick={() => {setCurrentTab(() => "pending")}}>Pending invites</button>
-          <button className={checkIfActive("sent", "BTN")} onClick={() => {setCurrentTab(() => "sent")}}>Sent invites</button>
+          <button
+            className={checkIfActive("find", "BTN")}
+            onClick={() => {
+              setCurrentTab(() => "find");
+            }}
+          >
+            Find
+          </button>
+          <button
+            className={checkIfActive("your", "BTN")}
+            onClick={() => {
+              setCurrentTab(() => "your");
+            }}
+          >
+            Your friends
+          </button>
+          <button
+            className={checkIfActive("pending", "BTN")}
+            onClick={handlePending}
+          >
+            Pending invites
+          </button>
+          <button className={checkIfActive("sent", "BTN")} onClick={handleSent}>
+            Sent invites
+          </button>
         </div>
         <div className={styles.tabs}>
           <div className={checkIfActive("find", "TAB")} id={"find"}>
-
+            <input type="text" placeholder="Find by username" />
+            <button>Search</button>
           </div>
-          <div className={checkIfActive("your", "TAB")} id={"your"}>
-          </div>
+          <div className={checkIfActive("your", "TAB")} id={"your"}></div>
           <div className={checkIfActive("pending", "TAB")} id={"pending"}>
-          
+            {friendsRequests.map((fr) => {
+              return <FriendRequest isPending={true} key={fr.id} {...fr} />;
+            })}
           </div>
           <div className={checkIfActive("sent", "TAB")} id={"sent"}>
-            
+            {friendsRequests.map((fr) => {
+              <FriendRequest isPending={false} key={fr.id} {...fr} />;
+            })}
           </div>
         </div>
       </div>
